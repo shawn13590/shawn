c8a013f0b2a39febeef9ce2d5aa91db9ab08d5e7
shawn13590 <111570449+shawn13590@users.noreply.github.com>
2022-08-19 15:48:26 +0530

    Add files via upload

---
+++ whatsapp-autoforward-message-main/whatsapp-autoforward-message-main/dist/run-me.bat
@@ -0,0 +1,3 @@
+Echo "Welcome to whatsapp Hack"
+
+WhatsAppAutoForward.exe --source "{{Abhijeet Karande (Grab)}}" --sourceIsGroup True --target "{{GRAB Infra Internal }}" --prefix "{{Test Message}}"
